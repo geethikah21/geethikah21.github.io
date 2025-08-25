7264b4d9c8a1aabf4aa654e69bf202433e1fe4f7
geethikah21 <geethikah21@gmail.com>
2025-08-25 03:13:22 +0000

    Deploying to gh-pages from @ geethikah21/geethikah21.github.io@6ca440d7d26facd93affdd3ff2cf812fd8e385ed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,72 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "news-texas-robotics-including-the-robocup-soccer-team-was-featured-on-the-austin-american-statesman",
           title: 'Texas Robotics (including the RoboCup soccer team) was featured on the Austin American...',
           description: "",
           section: "News",},{id: "news-i-graduated-from-ut-austin-with-a-bs-ms-in-computer-science",
           title: 'I graduated from UT Austin with a BS/MS in Computer Science!',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-spot-simulator",
+          title: 'Spot Simulator',
+          description: "Summer Fellowship Project - Simulating Spot gRPC Protocols",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-spot-social-navigation",
+          title: 'Spot Social Navigation',
+          description: "Understanding Human Perceptions of Quadruped Robots",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-enabling-block-stacking-intuition-in-robots",
+          title: 'Enabling Block-Stacking Intuition in Robots',
+          description: "Robot Learning Course Project",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-hydration-monitoring-application",
+          title: 'Hydration Monitoring Application',
+          description: "Human Sensing/Signals Course Project",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-robocup-standard-platform-league",
+          title: 'RoboCup Standard Platform League',
+          description: "Soccer Robot League",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-replication-study-for-multi-agent-path-finding-algorithm",
+          title: 'Replication Study for Multi-Agent Path Finding Algorithm',
+          description: "Planning, Search, and Reasoning Under Uncertainty Course Project",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-a-search",
+          title: 'A* Search',
+          description: "Planning, Search, and Reasoning Under Uncertainty Course Assignment",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
+            },},{id: "projects-hand-gesture-recognition",
+          title: 'Hand Gesture Recognition',
+          description: "Human Sensing/Signals Course Assignment",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
+            },},{id: "projects-mfcc-feature-extraction-pipeline",
+          title: 'MFCC Feature Extraction Pipeline',
+          description: "Spoken Language Technologies Course Assignment",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
